9a51b85fb31e3757fc770633c0811a2cba842e99
rishitsharmapec <119403588+rishitsharmapec@users.noreply.github.com>
2024-03-19 17:32:13 +0530

    Add files via upload

---
+++ user/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("dashboard-index")
+    else:
+        form = UserCreationForm()
+    return render(request, "user/register.html", {"form": form})
